b505027e438c662d7a85b8cafdcc1043b5edb615
Mohammed Saleh <supertechman007@gmail.com>
2024-02-14 23:40:18 +0200

    add 2048 game page

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function under_work_msg() {
+    alert("Under Construction")
+}
+
+function menu_popup(){
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("dark").style.display = "block";
+    document.getElementById("menu-open").style.display = "none";
+}
+
+function menu_popup_close(){
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("dark").style.display = "none";
+    document.getElementById("menu-open").style.display = "block";
+    }
+
